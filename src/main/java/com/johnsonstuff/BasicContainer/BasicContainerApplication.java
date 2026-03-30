2aec9e57185912a64fbea35ae5ce7c88ab56d061
Phil Johnson <phil.eric.johnson@gmail.com>
2026-03-29 22:30:20 -0500

    [DevOps-005] Ugly Static Versioning for now

--- src/main/java/com/johnsonstuff/BasicContainer/BasicContainerApplication.java
+++ src/main/java/com/johnsonstuff/BasicContainer/BasicContainerApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BasicContainerApplication {
 
+    // This is the main entry point for the Spring Boot application.
 	public static void main(String[] args) {
 		SpringApplication.run(BasicContainerApplication.class, args);
 	}
